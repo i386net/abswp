00e5ff1e42aa605f4384d6b2fa0f662fff8bb77b
dk_imlight <dk@i386.me>
2018-08-15 15:22:04 +0300

    regex test

--- chap8/practice/regex_search.py
+++ chap8/practice/regex_search.py
@@ -1,12 +1,19 @@
-# open all txt files and find there string with user's regex
-#  ввод регулярного выражения
-# получить список файлов в директории
-# пройтись по списку файлов
-# если результат регулярного выражения подходит скопировать в список
-# вывести список совпадений на экран или сообщение, что ничего не найдено
-
 import os
 import re
+matched_strings = []
+#  ввод регулярного выражения
+#message = input(u'Enter you regex: ')
+user_regex = re.compile(input(r'Enter you regex: '))
 
-user_regex = input(r'Enter you regex: ')
+# получить список файлов в директории
+for file in os.listdir('./files/'):
+    if file[-3:] == 'txt':
+        # пройтись по списку файлов
+        with open(os.path.join('./files/', file), 'r') as file_obj:
+            file_content = file_obj.read()
 
+            ms = user_regex.findall(file_content)
+            # если результат регулярного выражения подходит скопировать в список
+            matched_strings.append(ms)
+# вывести список совпадений на экран или сообщение, что ничего не найдено
+print(matched_strings)
