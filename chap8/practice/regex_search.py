49b4a4ad06d66b33087133b644de5bcfc2e12463
DK <i386.net@gmail.com>
2018-08-16 07:40:44 +0300

    fixed regexp

--- chap8/practice/regex_search.py
+++ chap8/practice/regex_search.py
@@ -1,5 +1,6 @@
 import os
 import re
+# test [\w\.-]+@[\w\.-]+
 matched_strings = []
 # ввод регулярного выражения
 user_regex = re.compile(input('Enter you regex: '))
@@ -8,9 +9,10 @@ user_regex = re.compile(input('Enter you regex: '))
 for file in os.listdir('./files/'):
     if file[-3:] == 'txt':
         # пройтись по списку файлов
-        print(file)
+
         with open(os.path.join('./files/', file), 'r') as file_obj:
             file_content = file_obj.read()
+
         matched = user_regex.findall(file_content)
         # если результат регулярного выражения подходит скопировать в список
         matched_strings.append(matched)
