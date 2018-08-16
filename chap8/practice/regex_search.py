4ae2591ccf6525aca9534e7ce48034ef3b3bb4e2
DK <i386.net@gmail.com>
2018-08-16 06:46:26 +0300

    fixes

--- chap8/practice/regex_search.py
+++ chap8/practice/regex_search.py
@@ -1,18 +1,18 @@
 import os
 import re
 matched_strings = []
-#  ввод регулярного выражения
-#message = input(u'Enter you regex: ')
-user_regex = re.compile(input(r'Enter you regex: '))
+# ввод регулярного выражения
+user_regex = re.compile(input('Enter you regex: '))
 
 # получить список файлов в директории
 for file in os.listdir('./files/'):
     if file[-3:] == 'txt':
         # пройтись по списку файлов
+        print(file)
         with open(os.path.join('./files/', file), 'r') as file_obj:
             file_content = file_obj.read()
-            matched = user_regex.findall(file_content)
-            # если результат регулярного выражения подходит скопировать в список
-            matched_strings.append(matched)
+        matched = user_regex.findall(file_content)
+        # если результат регулярного выражения подходит скопировать в список
+        matched_strings.append(matched)
 # вывести список совпадений на экран или сообщение, что ничего не найдено
 print(matched_strings)
