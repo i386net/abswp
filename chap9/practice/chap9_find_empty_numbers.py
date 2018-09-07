c464d589cd11ccdf892072153e5d9dd3cf7c26ee
DK <i386.net@gmail.com>
2018-09-07 16:19:12 +0300

    rename

--- chap9/practice/chap9_find_empty_numbers.py
+++ chap9/practice/chap9_find_empty_numbers.py
@@ -3,16 +3,16 @@ import os
 import re
 
 name_regex = re.compile(r'(spam)(\d{3})(\.txt)')
-spam_files = []
+spam_files_nums = []
 
 for file in os.listdir('./spam_dir'):
     file_with_num = name_regex.search(file)
     try:
-        spam_files.append(file_with_num.group(2))
+        spam_files_nums.append(file_with_num.group(2))
     except AttributeError:
         pass
-spam_files.sort()
-for s in spam_files:
+spam_files_nums.sort()
+for s in spam_files_nums:
     print(s)
 
 
