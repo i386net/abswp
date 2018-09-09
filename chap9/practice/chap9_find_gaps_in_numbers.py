5fedc2e8ffa369b4c16a028fc2fe57b52aa65ad9
DK <i386.net@gmail.com>
2018-09-09 13:35:56 +0300

    done with enumerate

--- chap9/practice/chap9_find_gaps_in_numbers.py
+++ chap9/practice/chap9_find_gaps_in_numbers.py
@@ -1,37 +1,16 @@
 import os
 import re
 
-name_regex = re.compile(r'(spam)(\d{3})(\.txt)')
+name_regex = re.compile(r'(spam)(\d+)(\.txt)')
 file_dir = './spam_dir'
 os.chdir(file_dir)
+file_dir_lst = os.listdir()
+file_dir_lst.sort()
 
-file_dir = os.listdir()
-file_dir.sort()
-'''
-for smaller_file in sorted(os.listdir('.')):
-    for bigger_file in sorted(os.listdir('.')):
-        smaller_file_num_rex = name_regex.search(smaller_file)
-        bigger_file_num_rex = name_regex.search(bigger_file)
-        smaller_num = smaller_file_num_rex.group(2)
-        bigger_num = bigger_file_num_rex.group(2)
-        if int(bigger_num) - int(smaller_num) >= 2:
-            new_num = int(smaller_num) + 1
-            new_name = bigger_file.replace(bigger_num, '00' + str(new_num))
-            #os.replace(file, new_name)
-            print(smaller_file)
-            print(bigger_file)
-            print(bigger_file, new_name)
-'''
-
-for x in range(len(file_dir)):  # smaller num
-    for y in range(len(file_dir)):  # bigger num
-        smaller_file_num_rex = name_regex.search(file_dir[x])
-        bigger_file_num_rex = name_regex.search(file_dir[y])
-        smaller_num = smaller_file_num_rex.group(2)
-        bigger_num = bigger_file_num_rex.group(2)
-        if int(bigger_num) - int(smaller_num) > 1:
-            new_num = int(smaller_num) + 1
-
-            new_name = file_dir[y].replace(bigger_num, '00' + str(new_num))
-            print('{} {} -> {}'.format(file_dir[x], file_dir[y], new_name))
+for i, file in enumerate(file_dir_lst, start=1):
+    num_r = name_regex.search(file)
+    num = num_r.group(2)
+    new_name = file.replace(num, '00{}'.format(i))
+    print(i, new_name)
+    os.rename(file, new_name)
 
