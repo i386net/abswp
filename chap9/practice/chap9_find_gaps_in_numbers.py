fca3fdae6bbeb4888b7eb13f24833ad9a8cba2d4
DK <i386.net@gmail.com>
2018-09-09 17:27:14 +0300

    correct code

--- chap9/practice/chap9_find_gaps_in_numbers.py
+++ chap9/practice/chap9_find_gaps_in_numbers.py
@@ -10,7 +10,8 @@ file_dir_lst.sort()
 for i, file in enumerate(file_dir_lst, start=1):
     num_r = name_regex.search(file)
     num = num_r.group(2)
-    new_name = file.replace(num, '00{}'.format(i))
-    print(i, new_name)
-    os.rename(file, new_name)
+    if i != int(num):
+        new_name = file.replace(num, '00{}'.format(i))
+        print(i, new_name)
+        os.rename(file, new_name)
 
