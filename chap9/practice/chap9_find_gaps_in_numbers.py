f4a7a8af2bad30cfa6ae01622b164aa02b27f857
DK <i386.net@gmail.com>
2018-09-09 08:58:32 +0300

    wrong code commented

--- chap9/practice/chap9_find_gaps_in_numbers.py
+++ chap9/practice/chap9_find_gaps_in_numbers.py
@@ -4,15 +4,18 @@ import re
 name_regex = re.compile(r'(spam)(\d{3})(\.txt)')
 file_dir = './spam_dir'
 os.chdir(file_dir)
-for prev_file in sorted(os.listdir('.')):
-    for file in sorted(os.listdir('.')):
-        prev_file_num_rex = name_regex.search(prev_file)
-        file_num_rex = name_regex.search(file)
-        prev_num = prev_file_num_rex.group(2)
-        file_num = file_num_rex.group(2)
-        if int(file_num) - int(prev_num) == 2:
-            new_num = int(prev_num) + 1
-            new_name = file.replace(file_num, '00' + str(new_num))
-            os.replace(file, new_name)
-            # print(file, new_name)
-
+'''
+for smaller_file in sorted(os.listdir('.')):
+    for bigger_file in sorted(os.listdir('.')):
+        smaller_file_num_rex = name_regex.search(smaller_file)
+        bigger_file_num_rex = name_regex.search(bigger_file)
+        smaller_num = smaller_file_num_rex.group(2)
+        bigger_num = bigger_file_num_rex.group(2)
+        if int(bigger_num) - int(smaller_num) >= 2:
+            new_num = int(smaller_num) + 1
+            new_name = bigger_file.replace(bigger_num, '00' + str(new_num))
+            #os.replace(file, new_name)
+            print(smaller_file)
+            print(bigger_file)
+            print(bigger_file, new_name)
+'''
